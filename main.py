d8409ead8e166314e8b2890729b5fd55c17dc6f0
Abbas Pathan <pathan.abbas10@gmail.com>
2022-07-11 22:41:39 +0530

    deploy setting changed

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from flask import Flask
+from flask_restful import Api,Resource,reqparse
 import json
 from cpu_benchmark import cpu_benchmark,check_name_cpu_benchmark
 from cinbench_r23 import cinebench_r23,check_name_cinebench_r23
+
 app=Flask(__name__)
+api=Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+class Contest(Resource):
+    def get(self,benchmark,cpu_name):
+        cpu_benchmark()
+        cinebench_r23()
+        if benchmark == 'cpu_benchmark':
+            return check_name_cpu_benchmark(cpu_name)
+        elif benchmark == 'cinebench_r23':
+            obj = check_name_cinebench_r23(cpu_name)
+            return f'{obj}'
 
-@app.route('/api/<string:benchmark>/<string:cpu_name>')
-def print_bench(benchmark,cpu_name):
-    cpu_benchmark()
-    cinebench_r23()
-    if benchmark=='cpu_benchmark':
-        return check_name_cpu_benchmark(cpu_name)
-    elif benchmark=='cinebench_r23':
-        obj=check_name_cinebench_r23(cpu_name)
-        return f'{obj}'
+
+api.add_resource(Contest, "/api/<string:benchmark>/<string:cpu_name>")
 
 
 if __name__ == '__main__':
